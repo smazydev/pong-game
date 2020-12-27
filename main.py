ab8f69b582ff5d985a99415462e8d81827e46cc5
smazydev <alizafar1437@gmail.com>
2020-12-27 17:31:04 +0500

    added scoreboard class, increase movement speed on hit functionality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
 from ball import Ball
+from scoreboard import Scoreboard
 import time
 
 R_POSITION = (350,0)
@@ -17,6 +18,7 @@ screen.tracer(0)
 r_paddle = Paddle(R_POSITION)
 l_paddle = Paddle(L_POSITION)
 ball = Ball()
+scoreboard = Scoreboard()
 
 
 screen.listen()
@@ -29,7 +31,7 @@ screen.onkey(l_paddle.go_down,"s")
 game_is_on = True
 
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(ball.move_speed)
     screen.update()
     ball.move()
 
@@ -39,7 +41,14 @@ while game_is_on:
     #Detect collisions with right paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    #Detect when right paddle misses 
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.increase_left_point()
 
-
+    #Detect when left paddle misses
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.increase_right_point()
 
 screen.exitonclick()
